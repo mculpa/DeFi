5a9222a152dc61f11c01a9400c6726c4f441cc7a
Mak <antonenko.makar123@gmail.com>
2025-07-16 19:29:16 -0400

    DeployDsc script implementation for future unit tests

---
+++ script/DeployDsc.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedSC} from "../src/DecentralizedSC.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDsc is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralizedSC, DSCEngine) {
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerPrivateKey) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        vm.startBroadcast();
+        DecentralizedSC dsc = new DecentralizedSC();
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(dscEngine));
+        vm.stopBroadcast();
+        return (dsc, dscEngine);
+    }
+}
